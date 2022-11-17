d87403b6cf7df94adaaecb1057e07643c4fe3006
Eddyhotkey <rshdbairamov@gmail.com>
2022-11-17 22:16:23 +0100

    The mobile-navigation has been programmed.

--- files/www_rashidbairamov_com/src/scripts/scripts.js
+++ files/www_rashidbairamov_com/src/scripts/scripts.js
@@ -1,8 +1,19 @@
+'use strict'
+/*
+    01_navigation_widget;
+    Animation für navigation icon;
+ */
 
-$('#nav-icon2').click(function(){
-    $(this).toggleClass('open');
-    console.log('click');
+$( document ).ready( function(){
+
+    const navigation_icon = $( '#nav_icon' );
+    const navigation_overlay = $( '.mobile_menu_overlay' );
+
+    $( navigation_icon ).click( function(){
+        $( this ).toggleClass( 'open' );
+    });
+
+    $( navigation_overlay ).click( function ( ) {
+        $( navigation_icon ).removeClass( 'open' );
+    });
 });
-
-
-console.log('hallo');
