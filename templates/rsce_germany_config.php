85237a283e3c0163533f25813912c0222678c282
Eddyhotkey <rshdbairamov@gmail.com>
2022-11-22 21:26:51 +0100

    The backend template has benn added.

---
+++ templates/rsce_germany_config.php
@@ -0,0 +1,22 @@
+<?php
+
+######################################
+### Created by Rashid Bairamov #######
+### https://www.rashidbairamov.de ####
+######################################
+
+return array(
+    'label' => array(
+        'Teaser-Element: das Zitat',
+    ),
+    'types' => array('content'),
+    'contentCategory' => 'RSCE: Inhalt',
+    'beTemplate' => 'be_wildcard',
+    'standardFields' => array( 'cssID' ),
+    'wrapper' => array(
+        'type' => 'none',
+    ),
+    'fields' => array(
+
+    ),
+);
