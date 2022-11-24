7132b58dcfffee54f68f39ffeadc369156cd9015
Eddyhotkey <rshdbairamov@gmail.com>
2022-11-24 18:06:33 +0100

    The backend template has been configured.

--- templates/rsce_germany_config.php
+++ templates/rsce_germany_config.php
@@ -7,7 +7,7 @@
 
 return array(
     'label' => array(
-        'Teaser-Element: das Zitat',
+        'Teaser-Element: das Zitat mit Autoren',
     ),
     'types' => array('content'),
     'contentCategory' => 'RSCE: Inhalt',
@@ -18,5 +18,28 @@ return array(
     ),
     'fields' => array(
 
+        ### GROUP ###
+        'quote_group' => array(
+            'label' => array('Das Zitat', 'Hier könnten Sie Texte mit Autoren hinzufügen'),
+            'inputType' => 'group',
+        ),
+
+        ### TEXT ###
+        'quote' => array(
+            'label' => array(
+                'de' => array('ein Zitat:', 'Hier können Sie einen Text eingeben'),
+            ),
+            'inputType' => 'textarea',
+            'eval' => array('tl_class' => 'w50 clr'),
+        ),
+
+        ### TEXT ###
+        'autor' => array(
+            'label' => array(
+                'de' => array('der Autor:', 'Hier können Sie einen Text eingeben'),
+            ),
+            'inputType' => 'textarea',
+            'eval' => array('tl_class' => 'w50 clr'),
+        ),
     ),
 );
